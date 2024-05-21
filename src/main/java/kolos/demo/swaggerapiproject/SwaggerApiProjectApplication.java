e69196045be1cf93c9e6f8f19c7e382ccb488d09
BigMoistLochu <krok.konrad1@gmail.com>
2024-05-21 22:25:12 +0200

    add project to repo

---
+++ src/main/java/kolos/demo/swaggerapiproject/SwaggerApiProjectApplication.java
@@ -0,0 +1,13 @@
+package kolos.demo.swaggerapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kolos/demo/swaggerapiproject/SwaggerApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kolos.demo.swaggerapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
